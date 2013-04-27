3ca459ab6bd87da83da03e7159b2f02f7a5653ad
Pascal <Zero@Ares.fritz.box>
2013-04-27 16:52:28 +0200

    entity for Lesson

---
+++ SchulplanerByJAMP/src/EPS/AppEW/SchulplanerByJAMP/entity/Lesson.java
@@ -0,0 +1,44 @@
+package EPS.AppEW.SchulplanerByJAMP.entity;
+
+public class Lesson {
+	
+	private String name;
+	
+	private String teacher;
+	
+	private String room;
+	
+	private String note;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+
+	public String getRoom() {
+		return room;
+	}
+
+	public void setRoom(String room) {
+		this.room = room;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+}
