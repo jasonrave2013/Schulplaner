0ce20bd999fcee6d01449a025dc6c7f0eb2a5ea2
jasonrave2013 <devan4mail90-4@gmx.de>
2013-05-01 17:53:43 +0200

    Home Activity Options Activity

--- SchulplanerByJAMP/src/EPS/AppEW/SchulplanerByJAMP/Home.java
+++ SchulplanerByJAMP/src/EPS/AppEW/SchulplanerByJAMP/Home.java
@@ -3,15 +3,25 @@ package EPS.AppEW.SchulplanerByJAMP;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import EPS.AppEW.SchulplanerByJAMP.Options;
+import EPS.AppEW.SchulplanerByJAMP.R;
+import android.view.View;
+import android.content.Intent;
 
 public class Home extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home); //Verknüpfung der Klasse mit der Layoutdatei
     }
-
+    
+    public void OpenOptions_OnClick(View v)
+    {
+    	Intent myIntent = new Intent(v.getContext(),
+    	  Options.class); //v.getContext=Referenz auf den gerade ausgeführten View
+    	startActivityForResult(myIntent, 0);
+    } 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
