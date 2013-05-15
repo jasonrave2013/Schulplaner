d16c8d27df6e4785c4abd3a47a24341194194033
Christian Kerker <christian.kerker@stud.fh-luebeck.de>
2013-05-15 12:39:09 +0200

    next try

---
+++ SchulplanerByJAMP/src/EPS/AppEW/SchulplanerByJAMP/FachViewAdapter.java
@@ -0,0 +1,40 @@
+package EPS.AppEW.SchulplanerByJAMP;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class FachViewAdapter extends BaseAdapter {
+
+	private Context context;
+	
+	public FachViewAdapter(Context c) {
+		context = c;
+	}
+	
+	@Override
+	public int getCount() {
+		
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
